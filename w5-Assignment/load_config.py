0344652e08d0ad693c1c69b881f8e1e0d0f39b31
Umair Yousif <37881515+umairyousif239@users.noreply.github.com>
2025-09-22 23:11:16 +0500

    config file adjustments

--- w5-Assignment/load_config.py
+++ w5-Assignment/load_config.py
@@ -1,16 +1,17 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
+
 load_dotenv()
 
 class Config:
-    GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
-    HUGGINGFACE_API_KEY = st.secrets.get("HUGGINGFACE_API_KEY", os.getenv("HUGGINGFACE_API_KEY"))
-    
+    GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY") or os.getenv("GEMINI_API_KEY")
+    HUGGINGFACE_API_KEY = st.secrets.get("HUGGINGFACE_API_KEY") or os.getenv("HUGGINGFACE_API_KEY")
+
     if not GEMINI_API_KEY:
-        raise ValueError("Missing GEMINI_API_KEY in .env")
+        raise ValueError("Missing GEMINI_API_KEY in secrets or .env")
 
     if not HUGGINGFACE_API_KEY:
-        raise ValueError("Missing HUGGINGFACE_API_KEY in .env")
+        raise ValueError("Missing HUGGINGFACE_API_KEY in secrets or .env")
 
     print("configuration successful...")
