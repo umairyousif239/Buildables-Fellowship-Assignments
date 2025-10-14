52f72b8f4c328860faa7302b07742f4451239add
Umair Yousif <37881515+umairyousif239@users.noreply.github.com>
2025-10-14 23:10:19 +0500

    deletion of the code
    will rewrite the whole code

--- w6-and-w7- Assignments/week-6-document-automation-and-RAG/main.py
+++
@@ -1,7 +0,0 @@
-import json
-from utils.pdf_structure import structure_extracted_data
-
-pdf_path = "C:/Users/umair/Desktop/Misc/Final Year Project Proposal/FYP Proposal (AI Surveillance System).pdf"
-data = structure_extracted_data(pdf_path)
-
-print(json.dumps(data, indent=2, ensure_ascii=False))
